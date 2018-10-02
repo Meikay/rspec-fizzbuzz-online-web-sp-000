64569b815259e0c4a619e1bad87de071676a2f43
Meisuleen Kased <meisuleen@gmail.com>
2018-10-02 23:29:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
-  def fizzbuzz(int)
-    if int % 3 == 0
-      "Fizz"
-    end
-    if int % 5 == 0
-      "Buzz"
-    end
-  end
+  end  
 end
+  if int % 5 == 0
+      "Buzz"
+  end
+
