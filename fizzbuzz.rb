49abd3de147350888df743605adf3a1c9213e9e7
Meisuleen Kased <meisuleen@gmail.com>
2018-10-02 23:35:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(3)
-  if 3 % 3 == 0
-    "Fizz"
+def fizzbuzz(fizz_3)
+  if fizz_3 % 3 == 0
+    return "Fizz"
   end
 end
   
