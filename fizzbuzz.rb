5aaf478308153c16511955f8836f63e6e1bf022d
Meisuleen Kased <meisuleen@gmail.com>
2018-10-02 23:30:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(3)
+  if 3 % 3 == 0
     "Fizz"
   end
 end
-  if int % 5 == 0
+  if 5 % 5 == 0
       "Buzz"
   end
